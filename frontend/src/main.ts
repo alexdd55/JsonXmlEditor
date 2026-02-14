adc5b4987a06e3ae09bd48f2386c17a24a1f04ce
Alex M. <alex.marquardt@gmail.com>
2026-02-14 21:31:23 +0100

    make code work

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 import App from './App.svelte'
+import { mount } from 'svelte'
 
 const target = document.getElementById('app')
 if (target) {
-  new App({ target })
+  mount(App, { target })
 }
